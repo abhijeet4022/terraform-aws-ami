9fe36cc7c3f89652dee2023dbab5b95ab63c0445
abhijeet4022 <abhijeet4022@gmail.com>
2024-11-22 09:19:27 +0530

    AMI Creation

--- main.tf
+++ main.tf
@@ -12,27 +12,31 @@ terraform {
   }
 }
 
-# Take one AMI to create Instance
+# Fetch the latest CentOS AMI
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
-  owners      = [973714476881]
+  owners      = ["973714476881"]
 }
 
-# Fetch the SG
+# Fetch the security group
 data "aws_security_group" "sg" {
   name = "allow-all"
 }
 
-# Create instance
+# Create an EC2 instance
 resource "aws_instance" "ec2" {
   instance_type          = "t3.small"
   ami                    = data.aws_ami.ami.image_id
   vpc_security_group_ids = [data.aws_security_group.sg.id]
-  tags                   = { Name = "ami" }
+  tags = {
+    Name        = "roboshop-instance"
+    Environment = "DevOps"
+    Purpose     = "AMI Creation"
+  }
 }
 
-# Install the software.
+# Install required software on the instance
 resource "null_resource" "provisioner" {
   provisioner "remote-exec" {
     connection {
@@ -42,15 +46,19 @@ resource "null_resource" "provisioner" {
       host     = aws_instance.ec2.private_ip
     }
     inline = [
-      "sudo yum install ansible python3.12-pip -y &> /tmp/userdata.log",
-      "sudo pip3.12 install  botocore boto3 &>> /tmp/userdata.log",
-      "sudo yum install bash-completion"
+      "sudo yum update -y &> /tmp/userdata.log",
+      "sudo yum install epel-release -y &>> /tmp/userdata.log",
+      "sudo yum install ansible python3.12-pip -y &>> /tmp/userdata.log",
+      "sudo pip3.12 install botocore boto3 &>> /tmp/userdata.log",
+      "sudo yum install bash-completion -y &>> /tmp/userdata.log"
     ]
   }
 }
 
-# Create AMI
-resource "aws_ami" "roboshop" {
+# Create an AMI from the instance
+resource "aws_ami_from_instance" "ami" {
   depends_on = [null_resource.provisioner]
-  name       = "roboshop-ami"
+  name       = "roboshop-ami-v1"
+  source_instance_id = "aws_instance.ami.id"
+  tags       = { Name = "roboshop-ami-v1" }
 }
